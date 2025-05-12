0ca633dda1df56e615108f1f427de0afa2d95897
unknown <antartyx@gmail.com>
2025-05-12 11:28:39 +0700

    koneksi

--- core/connection.php
+++ core/connection.php
@@ -1,11 +1,12 @@
 <?php
-$host = "172.17.0.2";       // atau IP seperti "127.0.0.1"
+$host = "127.0.0.1";       // atau IP seperti "127.0.0.1"
+$port = 3307;
 $username = "root";        // ganti sesuai user database kamu
 $password = "root";            // ganti sesuai password user database
 $database = "elearning_cpns"; // ganti dengan nama database kamu
 
 // Membuat koneksi
-$koneksi = new mysqli($host, $username, $password, $database);
+$koneksi = new mysqli($host, $username, $password, $database, $port);
 
 // Cek koneksi
 if ($koneksi->connect_error) {
